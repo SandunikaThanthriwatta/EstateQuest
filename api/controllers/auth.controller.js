319fbc844b7877b2c660fb690e71e763c9490d71
SandunikaThanthriwatta <s.thathriwatta07@gmail.com>
2024-05-26 00:00:48 +0530

    Create a middleware and a function to handle possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,20 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
 
-export const signup=async(req,res)=>{//async is used as await is used below
+export const signup=async(req,res,next)=>{//async is used as await is used below
 
    const{username,email,password}=req.body;
    const hashedPassword=bcryptjs.hashSync(password,10); //encrypt the password
-    const newUser=new User({username,email,password:hashedPassword});//save into the database
-    await newUser.save(); //prevent errors and wait until operation finishes
-    res.status(201).json("user created successfully");//giving a response
-
+   const newUser=new User({username,email,password:hashedPassword});//save into the database
+    
     //disply msgs and error in the client side
     try{
-        awaitUser.save(); 
+        await newUser.save(); 
         res.status(201).json('User created successfully!');
     }catch(error){
-        res.status(500).json(error.message);
+        next(error);
     }
 
 };
--- api/index.js
+++ api/index.js
@@ -14,6 +14,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 });
 const app=express();
+//allows to use json inputs to the server
+app.use(express.json());
 
 app.use(express.json());
 
@@ -25,3 +27,16 @@ app.listen(3000, ()=>{
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
+
+
+//middleware to handle errors
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode||500;   //status code get from the middleware
+    const message = err.message||'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    });
+
+})
