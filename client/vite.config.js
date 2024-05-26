1a80bb7b795ee625fb7e8abd4590eba3e9e48c81
SandunikaThanthriwatta <s.thathriwatta07@gmail.com>
2024-05-27 00:10:15 +0530

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //creating a server
+  server:{
+    //crreating a proxy
+    proxy:{
+      //when api/ path is given following path is added to the begining
+      '/api':{
+        target:'http://localhost:3000',
+        secure:false,
+      },
+    },
+
+  },
+
   plugins: [react()],
-})
+});
