a819e4706e3fadf7f0c53d023e5e88df4b9ff245
SandunikaThanthriwatta <s.thathriwatta07@gmail.com>
2024-07-24 23:01:01 +0530

    Create user update api route

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -17,7 +17,7 @@ const app=express();
 //allows to use json inputs to the server
 app.use(express.json());
 
-app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000!!');
